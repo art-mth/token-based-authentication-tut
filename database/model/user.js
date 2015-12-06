32bc4860b1347f264a1bcdb35057034664c7fd76
arthur <arthur.mathies@googlemail.com>
2015-12-06 15:20:46 -0800

    fix unique value in user database model

--- database/model/user.js
+++ database/model/user.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
+      unique: false
     }
   }, {
     timestamps: false
